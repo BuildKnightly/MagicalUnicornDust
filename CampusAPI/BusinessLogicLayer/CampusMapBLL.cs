499e7bd33a45b29b4c0c879d847c1175a3b05302
Kevin Schubach <kevin.schubach@gmail.com>
2018-03-30 07:49:13 -0700

    Added CampusMapBLL to hold shortest path algorithm

---
+++ CampusAPI/BusinessLogicLayer/CampusMapBLL.cs
@@ -0,0 +1,16 @@
+﻿using CampusAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CampusAPI.BusinessLogicLayer
+{
+  public class CampusMapBLL : CampusMap
+  {
+    public CampusMapBLL(CampusMap CampusMap)
+    {
+      base.nodes = CampusMap.nodes;
+    }
+  }
+}
